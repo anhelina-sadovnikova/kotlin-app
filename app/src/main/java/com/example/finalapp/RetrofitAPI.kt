832da45943e413a67f402d05664c6373e456a4de
Anhelina Sadovnikova <sadovanh@cvut.cz>
2024-05-31 23:21:33 +0200

    small changes

--- app/src/main/java/com/example/finalapp/RetrofitAPI.kt
+++ app/src/main/java/com/example/finalapp/RetrofitAPI.kt
@@ -6,11 +6,8 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface RetrofitAPI {
-
     @GET("pokemon")
-    fun getPokemonList(@Query("limit") limit: Int = 20, @Query("offset") offset: Int = 0): Call<PokemonResponse>
-
+    fun getPokemonList(@Query("limit") limit: Int = 1302, @Query("offset") offset: Int = 0): Call<PokemonResponse>
     @GET("pokemon/{name}")
     fun getPokemonDetail(@Path("name") name: String): Call<PokemonDetail>
-
 }
